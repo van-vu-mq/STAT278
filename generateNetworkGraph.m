4be74ab8a7a054fd97beff4ece773871df2fe995
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-08 10:34:53 +1000

    Add function to create visualisation of social network

---
+++ generateNetworkGraph.m
@@ -0,0 +1,19 @@
+function networkGraph = generateNetworkGraph(populationSize, socialNetworkSizeData, socialNetwork)
+% Graphs connects each person has with the rest of the population
+
+%%%% Graphing social network
+startNode = [];
+endNode = [];
+for person=1:populationSize
+    personFriendCount = socialNetworkSizeData(person,1);
+    for friendIndex=1:personFriendCount
+        startNode = [startNode, person];
+        endNode = [endNode, socialNetwork(person,friendIndex)];
+    end
+end
+
+
+networkGraph = graph(startNode, endNode);
+plot(networkGraph);
+
+end
