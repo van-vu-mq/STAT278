191416f8eeeca2d6955d36b4b6d0ad5b00d8184c
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-08 22:44:55 +1000

    Add function stub findNewPatients

---
+++ findNewPatients.m
@@ -0,0 +1,14 @@
+function newPatients = findNewPatients(populationList, listofSickPeople)
+% Logic to determine who becomes infected
+% Return a N x 1 array containing the index/ID of the people who has
+% contracted the disease.
+
+% We can consider updating the populationList and returning the whole
+% matrix instead
+%   OR
+% let the Main program handle the incubation period, expected sick duration
+% etc
+
+newPatients = [];
+
+end
