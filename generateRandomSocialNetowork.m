9a034378d4a12f669f3cc0b21412386734f1f182
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-08 10:35:09 +1000

    Rename function

---
+++ generateRandomSocialNetowork.m
@@ -0,0 +1,26 @@
+function socialNetwork = generateRandomSocialNetowork(populationSize, socialNetworkSizeData)
+
+%disp(socialNetworkSizeData);
+
+% 1 row per person
+% 1 column per friend of person
+socialNetwork = zeros(populationSize, max(socialNetworkSizeData));
+
+for person=1:populationSize
+    
+    friendCount = socialNetworkSizeData(person);
+    
+    for i=1:friendCount
+        % randomly pick from population
+        friend = randi(populationSize);
+        % make sure selected person is not already in the social circle
+        % make sure person is not a friend of themselves
+        while (friend == person || ismember(friend, socialNetwork(person,:)) == 1)
+            friend = ceil(rand()*populationSize);
+        end
+        % add friend to person's row in matrix
+        socialNetwork(person, i) = friend;
+    end
+end
+
+end
