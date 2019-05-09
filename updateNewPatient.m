83867edfad53959ecade9ee58b3e7b988edb9d97
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-09 23:45:51 +1000

    Change index system

--- updateNewPatient.m
+++ updateNewPatient.m
@@ -3,15 +3,30 @@ function personData = updateNewPatient(personData)
 
 diseaseData = getDiseaseData();
 
+
+% variables
+% age = 1;
+isSick = 2;
+% isVaccinated = 3;
+% socialNetworkSize = 4;
+% socialLevel = 5;
+% hospitalVisit = 6;
+sickDuration = 7;
+% daysSick = 8;
+incubationPeriod = 9;
+% hasSymptoms = 10;
+% atHome = 11;
+% previouslyInfected = 12;
+
 % update status to sick
-personData(1, 2) = 1;
+personData(1, isSick) = 1;
 
 
-%%%% make a function that calculates incubationa nd expected sickness
+%%%% make a function that calculates incubation and expected sickness
 %%%% duration.
 % generate expected duration of sickness 
-personData(1,7) = floor(normrnd(diseaseData(1,3), diseaseData(2,3)));
+personData(1,sickDuration) = ceil(normrnd(diseaseData(1,3), diseaseData(2,3)));
 
 % generate incubation period
-personData(1,8) = ceil(normrnd(diseaseData(1,2), diseaseData(2,2)));
+personData(1,incubationPeriod) = ceil(normrnd(diseaseData(1,2), diseaseData(2,2)));
 end
