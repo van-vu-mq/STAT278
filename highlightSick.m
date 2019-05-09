69e29f629a126587f8dc65b671d9ff594ea9de87
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-09 23:16:13 +1000

    Add. udpates cured/healthy people

--- highlightSick.m
+++ highlightSick.m
@@ -1,13 +1,25 @@
-function socialNetwork = highlightSick(plotted, socialNetwork, sickData)
-% Graphs connects each person has with the rest of the population
+function socialNetwork = highlightSick(plottedGraph, socialNetwork, sickData)
+% Colours the nodes of 
 
 
-sickList = [];
+% Separate population into two groups, sick and not sick
+sickList = [1, sum(sickData)];
+notSickList = [1, length(sickData)-sum(sickData)];
+sickIndex = 0;
+notSickIndex = 0;
 for person=1:length(sickData)
     if (sickData(person) == 1) 
-        sickList = [sickList, person];
+        sickIndex = sickIndex + 1;
+        sickList(sickIndex) = person;
+    else
+        notSickIndex = notSickIndex + 1;
+        notSickList(notSickIndex) = person;
     end
 end
-highlight(plotted, sickList, 'NodeColor', 'r');
+
+% colour sick people red
+% colour healthy people green
+highlight(plottedGraph, sickList, 'NodeColor', 'r');
+highlight(plottedGraph, notSickList, 'NodeColor', 'g');
 
 end
