6d65bfecccbc4d692bbb5b4f6227a757cf3e09e7
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-09 23:45:27 +1000

    Change network size to normal instead of uniform distribution

--- getPersonData.m
+++ getPersonData.m
@@ -21,16 +21,18 @@ personData(2, p_age) = 100;
 
 % Social network size / how many friends the person has
 % These are the people the person will interact with on a daily basis
-% min and max size of social circe
-personData(1, p_socialNetworkSize) = 1;
-% smaller value between 'max' people and 20% of population
-max = 20;
-personData(2, p_socialNetworkSize) = min(floor(populationSize*0.2), max);  
+% mu, sigma
+% smaller value between 'mu' people and 20% of population
+targetMean = 30;
+mu = min(floor(populationSize*0.1), targetMean);
+sigma = ceil(mu*0.2);
+personData(1, p_socialNetworkSize) = mu;
+personData(2, p_socialNetworkSize) = sigma;
 
 % How many people person will interact with on a daily basis
 % Social interaction will be with people drawn randomly from social
 % network
-% min, max as a percentage of socialNetworkSize
+% min as value, max as a percentage of socialNetworkSize
 personData(1, p_socialLevel) = 1;
 personData(2, p_socialLevel) = 0.2;
 
