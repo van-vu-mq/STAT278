b1ad10037f85078cf51c35d9dd36cb9c7b0d91ce
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-10 16:28:37 +1000

    Update findNewPatients.m

--- findNewPatients.m
+++ findNewPatients.m
@@ -1,14 +1,52 @@
-function newPatients = findNewPatients(populationList, listofSickPeople)
+function newPatients = findNewPatients(populationList, socialNetwork, listOfSickPeople)
 % Logic to determine who becomes infected
-% Return a N x 1 array containing the index/ID of the people who has
+% Return aa array containing the index/ID of the people who has
 % contracted the disease.
 
-% We can consider updating the populationList and returning the whole
-% matrix instead
-%   OR
-% let the Main program handle the incubation period, expected sick duration
-% etc
+% age = 1;
+% isSick = 2;
+isVaccinated = 3;
+socialNetworkSize = 4;
+socialLevel = 5;
+% hospitalVisit = 6;
+% symptomaticPeriod = 7;
+% daysSick = 8;
+% incubationPeriod = 9;
+% hasSymptoms = 10;
+% atHome = 11;
+% previouslyInfected = 12;
+
+diseaseData = getDiseaseData();
+d_infectionProbability = 1;
+% d_incubationPeriod = 2;
+% d_symptomaticPeriod = 3;
+% d_fatalityRate = 4;
 
 newPatients = [];
+
+for sickPerson=1:length(listOfSickPeople)
+    sickPersonIndex = listOfSickPeople(sickPerson,1);
+    mu = populationList(sickPersonIndex, socialLevel);
+    sigma = mu*0.2;
+    % people interact with different number of people daily
+    % determine how many people sick person will interact with
+    interactions = ceil(normrnd(mu, sigma));
+    
+    for i=1:interactions
+        % pick random friend from person's friend list
+        friend = randi(populationList(sickPersonIndex, socialNetworkSize));
+        % get friend's index/ID in the populationList
+        friendIndex = socialNetwork(sickPersonIndex, friend);
+        
+        % check friend is not vaccinated
+        if (populationList(friendIndex, isVaccinated) == 0)
+            % dice roll if person spreads the disease to friend
+            chance = rand();
+            if (chance < diseaseData(1,d_infectionProbability))
+                newPatients = [newPatients, friendIndex];
+            end
+        end
+    end
+end
 
 end
