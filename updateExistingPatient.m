87639c0ec9ecbae952cd81afc5d6bbca604b28d3
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-10 16:30:29 +1000

    Increment sick day counter, resets patient if timed out

--- updateExistingPatient.m
+++ updateExistingPatient.m
@@ -8,28 +8,50 @@ function personData = updateExistingPatient(personData)
 % update to display symptoms if incubation period is over
 
 
-% variables
+% Index of variables
 % age = 1;
-% isSick = 2;
+ isSick = 2;
 % isVaccinated = 3;
 % socialNetworkSize = 4;
 % socialLevel = 5;
 hospitalVisit = 6;
-% sickDuration = 7;
-% daysSick = 8;
-% incubationPeriod = 9;
+symptomaticPeriod = 7;
+daysSick = 8;
+incubationPeriod = 9;
 hasSymptoms = 10;
 atHome = 11;
 % previouslyInfected = 12;
 
-% person displays symptoms and is not already staying at home
-if (personData(1, hasSymptoms)==1 && personData(1, atHome)==0)
+% Update day tracker
+personData(1, daysSick) = personData(1, daysSick) + 1;
 
-    % determine whether person visits hospital
-    chance = rand();
-    if (chance < personData(1, hospitalVisit))
-        % update person based on hospital interaction
-        personData(1, :) = visitHospital(personData(1, :));
+
+totalSickDuration = personData(1, incubationPeriod) + personData(1, symptomaticPeriod);
+if (daysSick >= totalSickDuration)
+    % Person has served their sickness sentence, they are fine now
+    personData(1, isSick) = 0;
+    personData(1, incubationPeriod) = 0;
+    personData(1, symptomaticPeriod) = 0;
+    personData(1, daysSick) = 0;
+    personData(1, hasSymptoms) = 0;
+    
+elseif (personData(1, atHome == 0))
+    % person is still sick but not at home
+    
+    % check if person should be displaying symptoms 
+    if (personData(1, daysSick) > personData(1, incubationPeriod))
+        personData(1, hasSymptoms) = 1;
+    end
+    
+    % person has visible symptoms
+    if (personData(1, hasSymptoms)==1)
+
+        % determine whether person visits hospital
+        chance = rand();
+        if (chance < personData(1, hospitalVisit))
+            % update person based on hospital interaction
+            personData(1, :) = visitHospital(personData(1, :));
+        end
     end
 end
 
