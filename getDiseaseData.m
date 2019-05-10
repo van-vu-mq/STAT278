e9fc76b1b2c14c5c8143ee7b21c7423db01bd308
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-10 16:29:06 +1000

    Rename variables for clarity

--- getDiseaseData.m
+++ getDiseaseData.m
@@ -4,10 +4,10 @@ function diseaseData = getDiseaseData()
 
 
 % data indexing
-infectionProbability = 1;
-incubationPeriod = 2;
-symptomaticPeriod = 3;
-fatalityRate = 4;
+d_infectionProbability = 1;
+d_incubationPeriod = 2;
+d_symptomaticPeriod = 3;
+d_fatalityRate = 4;
 
 %%%%
 % row 1: mean
@@ -18,7 +18,7 @@ diseaseData = zeros(2, varCount);
 % How likely an unvaccinated person is to contract the disease from a
 % carrier
 % Percentage, 0-1
-diseaseData(1, infectionProbability) = 0.95;
+diseaseData(1, d_infectionProbability) = 0.95;
 
 % Incubation period
 % Number of days it takes for symptoms to appear. LogNormal distribution
@@ -28,8 +28,8 @@ v = 4;  % variance
 mu = log((m^2)/sqrt(v+m^2));
 sigma = sqrt(log(v/(m^2)+1));
 
-diseaseData(1, incubationPeriod) = mu;
-diseaseData(2, incubationPeriod) = sigma;
+diseaseData(1, d_incubationPeriod) = mu;
+diseaseData(2, d_incubationPeriod) = sigma;
 
 
 % Duration of symptoms - correlated to the active period of the diease
@@ -38,12 +38,12 @@ m = 10; % mean
 v = 2;  % variance
 mu = log((m^2)/sqrt(v+m^2));
 sigma = sqrt(log(v/(m^2)+1));
-diseaseData(1, symptomaticPeriod) = mu;
-diseaseData(2, symptomaticPeriod) = sigma;
+diseaseData(1, d_symptomaticPeriod) = mu;
+diseaseData(2, d_symptomaticPeriod) = sigma;
 
 % Fatality rate
 % Percentage
-diseaseData(1, fatalityRate) = 0.003;
+diseaseData(1, d_fatalityRate) = 0.003;
 
 
 
