a66719365ab18c416aed3abe9e4f459fa9fecf2e
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-11 13:17:21 +1000

    Update both people when adding connections

--- generateSocialNetwork.m
+++ generateSocialNetwork.m
@@ -1,25 +1,60 @@
 function socialNetwork = generateSocialNetwork(populationSize, socialNetworkSizeData)
+% Creates connections between people within the population
 
-%disp(socialNetworkSizeData);
-
+%===============================%
+%========= TO_DO LIST ==========%
+%===============================%
+% Bug list
+    % To prevent long search times for a person who has not yet reached
+    % assigned aquaintance count, function allows an N% assignment over the
+    % socialNetworkSize for each person.
+        % results in incorrect socialNetworkSize data
+        % temporary solution, Main will adjust each person's
+        % socialNetworkSize to match assigned social network
+    
+    
+%===== Initialise variable to hold data
 % 1 row per person
 % 1 column per friend of person
 socialNetwork = zeros(populationSize, max(socialNetworkSizeData));
+assignedCounter = zeros(populationSize, 1);
 
+% 
+overCapacityThreshold = 1.08;
+
+%===== Assign Aquaintances
 for person=1:populationSize
-    
-    friendCount = socialNetworkSizeData(person);
+    % how many to assign - how many already assigned
+    friendCount = 0;
+    % make sure not over limit
+    if (assignedCounter(person) < socialNetworkSizeData(person)*overCapacityThreshold)
+        friendCount = socialNetworkSizeData(person) - assignedCounter(person);
+    end
     
     for i=1:friendCount
         % randomly pick from population
         friend = randi(populationSize);
-        % make sure selected person is not already in the social circle
-        % make sure person is not a friend of themselves
-        while (friend == person || ismember(friend, socialNetwork(person,:)) == 1)
-            friend = ceil(rand()*populationSize);
+        
+        % make sure friend is not themself
+        % friend is not in person's social network
+        % person is not in friend's social network
+        
+        % will not make friend have too many people in social network
+            % temporary solution, allow 10% additional size
+        while (friend == person ...
+            || ismember(friend, socialNetwork(person,:)) == 1 ...
+            || ismember(person, socialNetwork(friend,:)) == 1 ...
+            || assignedCounter(friend) >= socialNetworkSizeData(friend)*overCapacityThreshold ...
+        )
+            friend = randi(populationSize);
         end
-        % add friend to person's row in matrix
-        socialNetwork(person, i) = friend;
+        % add friend to person's network
+        assignedCounter(person, 1) = assignedCounter(person,1) + 1;
+        socialNetwork(person, assignedCounter(person, 1)) = friend;
+        
+        % add person to friend's network
+        assignedCounter(friend, 1) = assignedCounter(friend,1) + 1;
+        socialNetwork(friend, assignedCounter(friend, 1)) = person;
     end
 end
 
