56b57e2a89d0ceca64bea939b4ab49b7d8ae8451
Zoe <zyiasemides@gmail.com>
2019-05-14 13:25:19 +1000

    correction

--- Main.m
+++ Main.m
@@ -142,7 +142,7 @@ for day=1:simulationPeriod
     % update information of people sick at the beginning of the day
     for person=1:length(listOfSickPeople)
         sickPersonID = listOfSickPeople(person);
-        populationList = updateExistingPatient(sickPersonID, populationList, socialNetworks);
+        populationList = updateExistingPatient(sickPersonID, populationList, socialNetwork);
     end
    
     %===== process/log data
