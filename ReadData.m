7252fb502d1e5b9fe5c8e86eca583d1612f829b7
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-14 15:21:49 +1000

    Read multiple files, combine to single file

--- ReadData.m
+++ ReadData.m
@@ -1,5 +1,23 @@
-filename = "Measles.txt";
-fileID = fopen(filename, 'r');
-data = fscanf(fileID, "%f");
-disp(data);
-fclose(fileID);
+simulationRepeats = 10;
+daysSimulated = 500;
+
+dataCollection = zeros(daysSimulated, simulationRepeats);
+
+for sim=1:simulationRepeats
+    filename = "Measles" + sim;
+    filename = filename + ".txt";
+    fileID = fopen(filename, 'r');
+    dataCollection(:, sim) = fscanf(fileID, "%f");
+    fclose(fileID);
+end
+
+filename = "Aggregate";
+filename = filename + ".txt";
+fileID = fopen(filename, 'w');
+% write the data
+for row = 1:size(dataCollection,1)
+    fprintf(fileID,'%g\t',dataCollection(row,:));
+    fprintf(fileID,'\n');
+end
+% fprintf(fileID, '%i', dataCollection);
+% fclose(fileID);
