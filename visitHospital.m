d95a6096d2c0850b74efc8fa8f490a744da09254
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2019-05-16 14:19:08 +1000

    Update to account non sick visitors

--- visitHospital.m
+++ visitHospital.m
@@ -6,10 +6,27 @@ function personData = visitHospital(personData)
 % 
 % By current design, person is defaulted 
 % to stay at home until all symptoms fade
- 
-atHome = 11;
 
-% make person stay at home
-personData(1, atHome) = 1;
+% age = 1;
+isSick = 2;
+isVaccinated = 3;
+% socialNetworkSize = 4;
+% socialLevel = 5;
+% hospitalVisit = 6;
+% symptomaticPeriod = 7;
+% daysSick = 8;
+% incubationPeriod = 9;
+% hasSymptoms = 10;
+atHome = 11;
+% previouslyInfected = 12;
+
+
+if(personData(1, isSick) == 1)
+    % if person is sick they are told to stay home
+    personData(1, atHome) = 1;
+else
+    % otherwise, they get vaccinated
+    personData(1, isVaccinated) = 1;
+end
 
 end
